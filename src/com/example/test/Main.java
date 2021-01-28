bf80fee9dfe0a597a99947a6245f72300cfea300
Boldeusz <mnpower@poczta.onet.pl>
2021-01-28 16:16:51 +0100

    update

--- src/com/example/test/Main.java
+++ src/com/example/test/Main.java
@@ -5,45 +5,44 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
-        int minNumber = Integer.MAX_VALUE;
-        int maxNumber = Integer.MIN_VALUE;
- //       boolean firstTime = true;    this is to flag
-        while (true){
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        //  boolean first = true;
+
+        while (true) {
 
             System.out.println("Enter number");
 
-            boolean hasNextInt = scanner.hasNextInt();
-            if (hasNextInt){
+            boolean isANInt = scanner.hasNextInt();
+
+            if (isANInt) {
                 int number = scanner.nextInt();
+                //  if(first){
+                //      first = false;
+                //     min = number;
+                //     max = number;
 
-//                if (firstTime){          // for the first time execute only then change to false!!!
-//                    firstTime = false;
-//                    minNumber = number;
-//                    maxNumber = number;
-//                }
-
-                System.out.println("Number is: " + number);
+                //  }
 
 
-
-                if ((number < minNumber)){
-                    minNumber = number;
-                }else if ((number > maxNumber)){
-                    maxNumber = number;
-                }else {
-                    break;
+                if (number > max) {
+                    max = number;
                 }
-                scanner.nextLine();
-
-            }else {
-                System.out.println("Invalid number, maxNumber is: "+ maxNumber + " and minNumber is: " + minNumber);
+                if (number < min) {
+                    min = number;
+                }
+            } else {
                 break;
+
             }
             scanner.nextLine();
-        }
-        scanner.close();
 
+
+        }
+        System.out.println("min= " + min + " , max= " + max);
+        scanner.close();
     }
 }
